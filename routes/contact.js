b24584ef8700bdae0541019c29a80a9b120932e4
MosesEtte0721 <ett20002@byui.edu>
2023-05-10 23:11:58 +0100

    CRUD

--- routes/contact.js
+++ routes/contact.js
@@ -6,9 +6,13 @@ const app = express();
 
 // gets multiple documents from a collection in the mongoDB
 router.get("/", mong.allDoc);
-
 // retrieved a single document from a collection in the mongoDB
 router.get("/:id", mong.singleDoc);
+// create and insert a new single document to a collection in the mongoDB
+router.post("/", mong.createContact)
 
+router.put("/:id", mong.updateContact);
+
+router.delete("/:id", mong.deleteContact);
 //
 module.exports = router;
